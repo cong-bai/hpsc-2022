a9c1a7599ee8419caef00b6b52bfe995005ab17b
cong-bai <cong.bai@outlook.com>
2022-05-23 01:56:59 +0900

    bug fixes

--- 10_io/17_phdf5_read.cpp
+++ 10_io/17_phdf5_read.cpp
@@ -23,9 +23,10 @@ int main (int argc, char** argv) {
   hsize_t NX = N[0], NY = N[1];
   hsize_t Nlocal[2] = {NX/dim[0], NY/dim[1]};
   hsize_t offset[2] = {mpirank / dim[0], mpirank % dim[0]};
-  hsize_t count[2] = {NX/dim[0], NY/dim[1]};
-  hsize_t stride[2] = {2,2};
-  hsize_t block[2] = {1,1}
+  for(int i=0; i<2; i++) offset[i] *= Nlocal[i] / 2;
+  hsize_t count[2] = {2, 2};
+  hsize_t stride[2] = {NX / dim[0], NY / dim[1]};
+  hsize_t block[2] = {NX / dim[0] / 2, NY / dim[1] / 2};
   hid_t localspace = H5Screate_simple(2, Nlocal, NULL);
   H5Sselect_hyperslab(globalspace, H5S_SELECT_SET, offset, stride, count, block);
   H5Pclose(plist);
